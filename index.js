ab4fe8e2f181e547fe13aa96e7859f9a58aa12be
muhtarudinsiregar <muhtarudinsiregar93@gmail.com>
2017-02-05 18:41:25 +0700

    remove uppercase

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var getFirstWords = function (sentence) {
   if (!sentence) return false;
 
   var splitSentence = sentence.split(" ");
-  var word = splitSentence[0].toUpperCase();
+  var word = splitSentence[0];
 
   return word;
 }
--- test.js
+++ test.js
@@ -2,8 +2,8 @@ const assert = require('assert');
 const gfw = require('./index.js');
 
 describe('test output', function() {
-  it("should return 'FIRST'", function() {
-    assert.equal('FIRST', gfw("First Words Test"));
+  it("should return 'First'", function() {
+    assert.equal('First', gfw("First Words Test"));
   });
 
   it("should return 'false' if param null", function() {

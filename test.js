31a044b344a0b080e75bf220f4806133530cc0d0
muhtarudinsiregar <muhtarudinsiregar93@gmail.com>
2017-02-09 10:18:04 +0700

    standard js fix

--- test.js
+++ test.js
@@ -1,13 +1,12 @@
-const assert = require('assert');
-const gfw = require('./index.js');
+const assert = require('assert')
+const gfw = require('./index.js')
 
-describe('test output', function() {
-  it("should return 'First'", function() {
-    assert.equal('First', gfw("First Words Test"));
-  });
+describe('test output', function () {
+  it("should return 'First'", function () {
+    assert.equal('First', gfw('First Words Test'))
+  })
 
-  it("should return 'false' if param null", function() {
-    assert.equal(false, gfw());
-  });
-
-});
+  it("should return 'false' if param null", function () {
+    assert.equal(false, gfw())
+  })
+})
